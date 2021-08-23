e66c9cbce88790c716224f63e06e722f64cac499
Simmieyungie <56706551+simmieyungie@users.noreply.github.com>
2021-08-23 15:35:38 +0100

    rfm table

---
+++ RFM/rfm.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 15:15:09 2021
+
+@author: SIMIYOUNG
+"""
+
+#import sys
+#sys.path.append(r'C:/Users/SIMIYOUNG/Documents/Python-Projects/RFM/PyRFM/RFM')
+
+#import RFM.recency
+
+
+
+#import recency, frequency, monetary
